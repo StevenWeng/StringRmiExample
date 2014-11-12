ac6755b04f86051551d5474c1298e2082af332da
StevenWeng <stevenweng@systex.com.tw>
2014-11-12 11:20:44 +0800

    init

---
+++ src/main/java/com/irsa/example/StringRmiExample/App.java
@@ -0,0 +1,13 @@
+package com.irsa.example.StringRmiExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
