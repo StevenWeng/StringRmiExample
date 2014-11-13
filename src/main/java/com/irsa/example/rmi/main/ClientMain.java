998264e20a47dc34938de20bbab59cde7aefe964
StevenWeng <stevenweng@systex.com.tw>
2014-11-13 16:57:45 +0800

    CallbackService 撞牆

--- src/main/java/com/irsa/example/rmi/main/ClientMain.java
+++ src/main/java/com/irsa/example/rmi/main/ClientMain.java
@@ -1,9 +1,13 @@
 package com.irsa.example.rmi.main;
 
+import java.util.Date;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.remoting.rmi.RmiServiceExporter;
 
+import com.irsa.example.rmi.Callback;
+import com.irsa.example.rmi.CallbackService;
 import com.irsa.example.rmi.RequestService;
 
 public class ClientMain {
@@ -14,6 +18,16 @@ public class ClientMain {
 		RequestService requestService = (RequestService) ctx.getBean("requestServiceProxy");
 		Double bmi = requestService.getBmi(177.7, 74.5);
 		System.out.println(bmi);
+		
+		CallbackService callbackService = (CallbackService) ctx.getBean("callbackServiceProxy");
+		callbackService.registry(new Callback() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void onReceiveTime(Date date) {
+				System.out.println("Call back Time: " + date);
+			}
+		});
 	}
 
 }
--- src/main/java/com/irsa/example/rmi/main/ServerMain.java
+++ src/main/java/com/irsa/example/rmi/main/ServerMain.java
@@ -4,13 +4,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.remoting.rmi.RmiServiceExporter;
 
+import com.irsa.example.rmi.CallbackService;
+import com.irsa.example.rmi.impl.CallbackServiceImpl;
+
 public class ServerMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("server-beans.xml");
-		RmiServiceExporter requestServiceExporter = (RmiServiceExporter) ctx.getBean("requestServiceExporter");
-		System.out.println(requestServiceExporter.getServiceInterface());
+		CallbackServiceImpl callbackService = (CallbackServiceImpl) ctx.getBean("callbackService");
+		callbackService.start();
 	}
 
 }
